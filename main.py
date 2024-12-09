7a919265a1d0a00ed30da2892310ac6989b748ba
Markizzann <186683058+Markizzann@users.noreply.github.com>
2024-12-09 20:51:29 +0300

    Работа Excel

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import pandas as pd
 
 script_dir = os.path.dirname(os.path.abspath(file))
 file_path = os.path.join(script_dir, 'phone_numbers.xlsx')
+
+df = pd.read_excel(file_path)
+df.to_excel(file_path, index=False)
