1734a3807807727e77befdb8461c405a669d6b71
Markizzann <186683058+Markizzann@users.noreply.github.com>
2024-12-09 21:02:43 +0300

    Чтение данных из Excel

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from openpyxl import load_workbook
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, 'phone_numbers.xlsx')
 output_file_path = os.path.join(script_dir, 'cleaned_phone_numbers.xlsx')
+df = pd.read_excel(file_path, engine='openpyxl')
