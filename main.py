7980e3c6686e9d6eda5669fddeb9ad8f815649bb
Markizzann <186683058+Markizzann@users.noreply.github.com>
2024-12-09 20:44:19 +0300

    Clean

--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-script_dir = os.path.dirname(os.path.abspath(file))
-file_path = os.path.join(script_dir, 'phone_numbers.xlsx')
-df = pd.read_excel(file_path)
-df.to_excel(file_path, index=False)
