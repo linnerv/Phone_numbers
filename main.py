c146694533f5463ce6310d70f62e61860452dd11
linnerv <polinavershinskaya@gmail.com>
2024-12-09 21:12:02 +0300

    супер-пупер код 2

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-## main стартовый модуль проекта
-# project_4
+import pandas as pd
+import os
+from openpyxl import load_workbook
 
-# Дан файл .xlsx с номерами телефонов. 
-# Напишите скрипт, который очищает телефоны от ненужных символов. Результат сохраните в новый файл с расширением .xlsx. 
-# Например, 
-# 8(645) 590-10-12 -> 86455901012.  
-
-# Используйте библиотеку pandas.
-
-# Ссылка на файл.
-# https://drive.google.com/drive/folders/1FpCRKyCTgAuWS9h2O0tWmJZ5erTPYxQ1?usp=sharing
+def process_row(row):
+    s = row['phone_number']
+    s_new = ''.join(filter(str.isdigit, str(s)))
+    row['phone_number'] = s_new
+    return row 
 
