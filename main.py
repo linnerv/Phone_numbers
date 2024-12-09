e3feac9bb99a86a7078aba65c72f85baf777da5f
linnerv <polinavershinskaya@gmail.com>
2024-12-09 20:56:02 +0300

    написала функцию

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import os
 
+def process_row(row):
+    s = row['phone_number']
+    s_new = ''.join(filter(str.isdigit, str(s)))
+    row['phone_number'] = s_new
+    return row
 
