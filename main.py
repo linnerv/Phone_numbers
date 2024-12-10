1d8f59936c61f3deed81a69c4ad73658857a4f48
Markizzann <186683058+Markizzann@users.noreply.github.com>
2024-12-10 09:41:45 +0300

    Конечная правка (добавила функцию)

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 import os
 from openpyxl import load_workbook
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, 'phone_numbers.xlsx')
-output_file_path = os.path.join(script_dir, 'cleaned_phone_numbers.xlsx')
-df = pd.read_excel(file_path, engine='openpyxl')
-df = df.apply(process_row, axis=1)
-df.to_excel(output_file_path, index=False, engine='openpyxl')
+def process_file():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, 'phone_numbers.xlsx')
+    output_file_path = os.path.join(script_dir, 'cleaned_phone_numbers.xlsx')
+    df = pd.read_excel(file_path)
+    df = df.apply(process_row, axis=1)
+    df.to_excel(output_file_path, index=False, engine='openpyxl')
