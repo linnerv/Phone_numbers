1d502e44fc709277cb287f771efa76b5fe2480f3
linnerv <polinavershinskaya@gmail.com>
2024-12-26 00:51:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 ## main стартовый модуль проекта
+
 from functions import process_file
 
+def main():
+    # TODO - добавь обработку ошибок 
+    process_file() 
 
 # инициализационный скрипт
 if __name__ == "__main__":
-    process_file()
+    main()
